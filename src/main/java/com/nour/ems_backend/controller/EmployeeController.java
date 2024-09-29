08a55106b799692fbd75829b2bd73fe023d47cf4
nour <174273417+NourDoesCode@users.noreply.github.com>
2024-09-29 12:39:56 +0100

    add post controller

---
+++ src/main/java/com/nour/ems_backend/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.nour.ems_backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nour.ems_backend.dto.EmployeeDTO;
+import com.nour.ems_backend.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    // Build add Employee rest API
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDTO);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
